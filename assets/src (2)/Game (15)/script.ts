c2e69a22db6f983ee3b0510d77bcc8e40894ef42
Olivier <harraps3.0@gmail.com>
2016-02-08 23:39:49 +0100

    Testing soundcloud API in superpowers
    just play a song from soundcloud using the URL of the Song
    ( Pika girl ! )

--- assets/src (2)/Game (15)/script.ts
+++ assets/src (2)/Game (15)/script.ts
@@ -1,6 +1,21 @@
 // http://stackoverflow.com/questions/18880715/texture-splatting-with-three-js
 // http://algoholic.eu/unity3d-indie-free-portal-effect-project-sources/
 
+declare let window;
+
+// we add a scripts from outside sources
+(function (){
+    let scripts : string[] = [
+        "http://connect.soundcloud.com/sdk.js" // We add the soundcloud SDK
+    ];
+    for( let script of scripts ){
+        let script_div = window.document.createElement('script');
+        script_div.type = "text/javascript";
+        script_div.src = script;
+        window.document.body.appendChild(script_div);
+    }
+})();
+
 let WORLD = Sup.Cannon.getWorld();
 WORLD.gravity.set( 0, -6000/Sup.Game.getFPS(), 0 );
 WORLD.defaultContactMaterial.friction = 0.1;
---
+++ assets/src (2)/Tests (72)/SoundCloud (73)/script.ts
@@ -0,0 +1,27 @@
+class SoundCloud extends Sup.Behavior {
+    
+    private static client_id : string = '29c3bd07f77f1de4398d905f91da4439'; // to get an ID go to http://developers.soundcloud.com/
+    private static SC;
+    
+    start() {
+        window.SC.initialize({client_id: SoundCloud.client_id});
+        SoundCloud.SC = window.SC;        
+        SoundCloud.SC.get('/resolve', {url: "https://soundcloud.com/s3rl/s3rl-pika-girl-radio-edit-emfa"}, function(url){
+            if( !url.errors ){
+                Sup.log(url);
+                if( url.kind == "playlist" ){
+                    
+                }else{
+                    SoundCloud.SC.stream('/tracks/'+url.id, function(sound){
+                        sound.start();
+                    });
+                }
+            }
+        });
+    }
+
+    update() {
+        
+    }
+}
+Sup.registerBehavior(SoundCloud);
